419859d986ce8337d147d9fc7c16e16e3fcaec70
Morten Noddeland <morten@noddeland.no>
2017-08-15 15:17:50 +0200

    Add random taglines

--- main.js
+++ main.js
@@ -3,13 +3,38 @@ function resizeCardHeight() {
 
   // We want the height to be equal to the width
   height = cards[0].offsetWidth + 'px';
-  console.log(height);
 
   for (card of cards) {
     card.style.height = height;
   }
 }
 
+function getRandomTagline() {
+  var taglines = [
+    "Dekker alle dine behov som UiB-student... bortsett fra kaffe",
+    "UiBs IT-tjenester? Gotta know 'em all!",
+    "Du och jag, UiB. Du och jag...",
+    "Alle lenkene du trenger... og et par til",
+    "Uhu-ggelig nyttige lenker",
+  ];
+
+  n = Math.floor((Math.random() * taglines.length));
+
+  return taglines[n];
+}
+
+function setTagline() {
+  tagline_string = getRandomTagline();
+  tagline_dom = document.getElementsByClassName('tagline')[0];
+
+  tagline_dom.innerHTML = tagline_string;
+}
+
 window.addEventListener("resize", resizeCardHeight);
 
-resizeCardHeight();
+function main() {
+  setTagline();
+  resizeCardHeight();
+}
+
+main();
