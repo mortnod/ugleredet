e2294787c69208c0545e8d932fe3bb1f3a9f5fa0
Morten Noddeland <morten@noddeland.no>
2017-08-17 10:32:19 +0200

    Add modal

--- main.js
+++ main.js
@@ -30,11 +30,42 @@ function setTagline() {
   tagline_dom.innerHTML = tagline_string;
 }
 
-window.addEventListener("resize", resizeCardHeight);
+function activateModal() {
+  // Get the modal
+  var modal = document.getElementById("modal-background");
+
+  // Get the button that opens the modal
+  var open_button = document.getElementById("open_modal_button");
+
+  // Get the (x) that closes the modal
+  var close_button = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  open_button.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks the close_button, close the modal
+  close_button.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
 
 function main() {
   setTagline();
+
   resizeCardHeight();
+  window.addEventListener("resize", resizeCardHeight);
+
+  activateModal();
 }
 
 main();
